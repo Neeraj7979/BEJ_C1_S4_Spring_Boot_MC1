89383cb22b18666af5528ec4ccea91c079db32a6
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-27 00:13:19 +0530

    Final commit

--- src/main/java/com/niit/bej/springbootc4mc1/SpringBootC4Mc1Application.java
+++ src/main/java/com/niit/bej/springbootc4mc1/SpringBootC4Mc1Application.java
@@ -1,13 +1,17 @@
 package com.niit.bej.springbootc4mc1;
 
+import com.niit.bej.springbootc4mc1.service.MessageService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootC4Mc1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootC4Mc1Application.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(SpringBootC4Mc1Application.class, args);
+		MessageService messageService = applicationContext.getBean(MessageService.class);
+		messageService.displayMessage();
 	}
 
 }
