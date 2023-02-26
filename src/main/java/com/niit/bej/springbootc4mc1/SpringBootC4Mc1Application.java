23bfdbd787695a32d667bf4295c3bba4cf35268a
Neeraj7979 <Imnsinha77@outlook.com>
2023-02-27 00:02:17 +0530

    Initial commit

---
+++ src/main/java/com/niit/bej/springbootc4mc1/SpringBootC4Mc1Application.java
@@ -0,0 +1,13 @@
+package com.niit.bej.springbootc4mc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootC4Mc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootC4Mc1Application.class, args);
+	}
+
+}
